cc5856af1d12f225ffc6cd854402531ced7f7c4a
“Rahul <rahul.gupta312002@gmail.com>
2024-10-27 15:41:52 +0530

    updated test_operations

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -3,12 +3,13 @@ from src.math_operations import add,sub,division
 def test_add():
     assert add(2,3)==5
     assert add(-1,1)==0
+    assert add(-1,-1)==-2
 
 def test_sub():
     assert sub(5,3)==2
     assert sub(3,3)==0
     assert sub(3,9)==-6
-    assert sub(-1,-1)==-2
+    assert sub(-1,-1)==0
 
 def test_divison():
     assert division(8,4)==2
